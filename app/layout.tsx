b4e28c4cd605160386da5ae3e6d57061df335ed9
Phil Crompton <p_crompton@hotmail.co.uk>
2026-03-17 15:12:14 +0000

    fix google analytics setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -36,20 +37,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      
-      {/* Google Analytics */}
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DM4FPV8B7M"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-DM4FPV8B7M');
-            `,
-          }}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DM4FPV8B7M"
+          strategy="afterInteractive"
         />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-DM4FPV8B7M', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
       </head>
 
       <body>{children}</body>
